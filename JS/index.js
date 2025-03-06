36b6e5319c7288d2da49cd6cf854277046f3c830
Yue Dong <ydong011@fiu.edu>
2025-03-06 12:56:54 -0600

    update

---
+++ JS/index.js
@@ -0,0 +1,55 @@
+/**
+ * @class
+ * what is javascript, ECMAScript
+ * javascript vs. nodejs vs. ECMAScript
+ *
+ * Primitive Data
+ * Object Data or reference Data
+ *
+ * coercion
+ * equality == vs. ===
+ *
+ * var vs. let vs. const
+ *
+ * oop: Object oriented programming in JS
+ * encapsulation; inheritance; Poly-morph-ism; abstraction;
+ * constructer function, prototype chain
+ *
+ * @class
+ * loop in JS: array, object
+ * MyForeach, MyMap, MyFilter, MyReduce
+ *
+ * destructure
+ *
+ * rest parameter vs. spread operator
+ *
+ * object copy
+ *
+ * @class
+ * iife
+ * closure
+ * currying
+ *
+ * this
+ * call, apply, bind
+ *
+ * arrow function
+ *
+ * event loop
+ *
+ * @class
+ * XHR
+ * callback function; callback hell
+ * Promise
+ * MyPromise
+ * MyFetch
+ *
+ * @class
+ * todolist:
+ *   MVC
+ *   NodeList vs. HTMLCollection
+ *   Event bubbling
+ *
+ * @class
+ * JQuery
+ */
